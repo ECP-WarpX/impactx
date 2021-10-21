158217adb1e44177a3d5bb291d5717d9caec69a0
Axel Huebl <axel.huebl@plasma.ninja>
2021-10-21 10:16:12 -0700

    Add particle structure (files)

---
+++ src/particles/ImpactXParticleContainer.cpp
@@ -0,0 +1,22 @@
+/* Copyright 2021 Axel Huebl, Andrew Myers
+ *
+ * This file is part of ImpactX.
+ *
+ * License: BSD-3-Clause-LBNL
+ */
+#include "ImpactXParticleContainer.H"
+
+namespace impactx
+{
+
+    /*
+    ImpactXParticleContainer::ImpactXParticleContainer()
+    {
+        // todo
+    }
+    */
+
+    void foo()
+    {} // placeholder
+
+} // namespace impactx
