cd38575e2691b400c15d3f79ef9d8448269675b6
Axel Huebl <axel.huebl@plasma.ninja>
2021-10-26 10:58:43 -0700

    Implement: ImpactXParticleContainer

--- src/particles/ImpactXParticleContainer.cpp
+++ src/particles/ImpactXParticleContainer.cpp
@@ -1,4 +1,4 @@
-/* Copyright 2021 Axel Huebl, Andrew Myers
+/* Copyright 2021 Axel Huebl
  *
  * This file is part of ImpactX.
  *
@@ -6,17 +6,79 @@
  */
 #include "ImpactXParticleContainer.H"
 
+#include <AMReX_AmrCore.H>
+#include <AMReX_ParallelDescriptor.H>
+#include <AMReX_ParticleTile.H>
+
+
 namespace impactx
 {
-
-    /*
-    ImpactXParticleContainer::ImpactXParticleContainer()
+    ImpactXParticleContainer::ImpactXParticleContainer (amrex::AmrCore* amr_core)
+        // TODO!! : amrex::ParticleContainer<0, 0, RealSoA::nattribs, IntSoA::nattribs>(amr_core->GetParGDB())
     {
-        // todo
+       SetParticleSize();
     }
-    */
 
-    void foo()
-    {} // placeholder
+    void
+    ImpactXParticleContainer::AddNParticles (int lev,
+                                             amrex::Vector<amrex::ParticleReal> const & x,
+                                             amrex::Vector<amrex::ParticleReal> const & y,
+                                             amrex::Vector<amrex::ParticleReal> const & z)
+    {
+        AMREX_ALWAYS_ASSERT_WITH_MESSAGE(lev == 0, "AddNParticles: only lev=0 is supported yet.");
+        AMREX_ALWAYS_ASSERT(x.size() == y.size());
+        AMREX_ALWAYS_ASSERT(x.size() == z.size());
 
+        // number of particles to add
+        int const np = x.size();
+
+        // have to resize here, not in the constructor because grids have not
+        // been built when constructor was called.
+        reserveData();
+        resizeData();
+
+        auto& particle_tile = DefineAndReturnParticleTile(0, 0, 0);
+
+        /* Create a temporary tile to obtain data from simulation. This data
+         * is then copied to the permanent tile which is stored on the particle
+         * (particle_tile).
+         */
+        using PinnedTile = amrex::ParticleTile<NStructReal, NStructInt, NArrayReal, NArrayInt,
+                amrex::PinnedArenaAllocator>;
+        PinnedTile pinned_tile;
+        pinned_tile.define(NumRuntimeRealComps(), NumRuntimeIntComps());
+
+        for (int i = 0; i < np; i++)
+        {
+            ParticleType p;
+            p.id() = ParticleType::NextID();
+            p.cpu() = amrex::ParallelDescriptor::MyProc();
+            p.pos(0) = x[i];
+            p.pos(1) = y[i];
+            p.pos(2) = z[i];
+            // write position, creating cpu id, and particle id
+            pinned_tile.push_back(p);
+
+            // write Real attributes (SoA) to particle initialized zero
+            DefineAndReturnParticleTile(0, 0, 0);
+
+            pinned_tile.push_back_real(RealSoA::px, np, 0.0);
+            pinned_tile.push_back_real(RealSoA::py, np, 0.0);
+            pinned_tile.push_back_real(RealSoA::pz, np, 0.0);
+            pinned_tile.push_back_real(RealSoA::t, np, 0.0);
+            pinned_tile.push_back_real(RealSoA::q_m, np, 0.0);
+            pinned_tile.push_back_real(RealSoA::w, np, 0.0);
+        }
+
+        /* Redistributes particles to their respective tiles (spatial bucket
+         * sort per box over MPI ranks)
+         */
+        auto old_np = particle_tile.numParticles();
+        auto new_np = old_np + pinned_tile.numParticles();
+        particle_tile.resize(new_np);
+        amrex::copyParticles(
+                particle_tile, pinned_tile, 0, old_np, pinned_tile.numParticles());
+        Redistribute();
+
+    }
 } // namespace impactx
