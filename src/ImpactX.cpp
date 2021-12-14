d95d0b51beae2dce2481a77c72760ba379034d1a
Remi Lehe <remi.lehe@normalesup.org>
2021-12-14 11:25:56 -0800

    Add first input script (#26)
    * Add empty input script
    
    * Read lattice elements
    
    * Added FODO cell
    
    * Add sbend
    
    * Update error message
    
    * Change name of the input file
    
    * Add documentation
    
    * Implement code review suggestions
    
    * Re-order STL includes
    
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>

--- src/ImpactX.cpp
+++ src/ImpactX.cpp
@@ -9,7 +9,11 @@
 #include "particles/Push.H"
 
 #include <AMReX.H>
+#include <AMReX_REAL.H>
 #include <AMReX_ParmParse.H>
+
+#include <string>
+#include <vector>
 
 
 namespace impactx
@@ -147,14 +151,36 @@ namespace impactx
         // make sure the element sequence is empty
         m_lattice.clear();
 
-        // add elements
-        //   FODO cell
-        m_lattice.emplace_back(Quad(1.0, 4.0));
-        m_lattice.emplace_back(Drift(0.5));
-        m_lattice.emplace_back(Quad(1.0, 4.0));
-        m_lattice.emplace_back(Drift(0.5));
-        //   a bending magnet
-        m_lattice.emplace_back(Sbend(0.5, 2.0));
+        // Parse the lattice elements
+        amrex::ParmParse pp_lattice("lattice");
+        std::vector<std::string> lattice_elements;
+        pp_lattice.queryarr("elements", lattice_elements);
+
+        // Loop through lattice elements
+        for (std::string const element_name : lattice_elements) {
+            // Check the element type
+            amrex::ParmParse pp_element(element_name);
+            std::string element_type;
+            pp_element.get("type", element_type);
+            // Initialize the corresponding element according to its type
+            if (element_type == "quad") {
+                amrex::Real ds, k;
+                pp_element.get("ds", ds);
+                pp_element.get("k", k);
+                m_lattice.emplace_back( Quad(ds, k) );
+            } else if (element_type == "drift") {
+                amrex::Real ds;
+                pp_element.get("ds", ds);
+                m_lattice.emplace_back( Drift(ds) );
+            } else if (element_type == "sbend") {
+                amrex::Real ds, rc;
+                pp_element.get("ds", ds);
+                pp_element.get("rc", rc);
+                m_lattice.emplace_back( Sbend(ds, rc) );
+            } else {
+                amrex::Abort("Unknown type for lattice element " + element_name + ": " + element_type);
+            }
+        }
 
         amrex::Print() << "Initialized element list" << std::endl;
     }
