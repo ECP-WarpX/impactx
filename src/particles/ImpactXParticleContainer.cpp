969872da3cc650f80bbce21b98e2d067b0bcf430
Axel Huebl <axel.huebl@plasma.ninja>
2021-12-14 19:19:44 -0800

    Min/Max/Std Position: from ABLASTR
    Move general min/max/std functions for particle containers into the
    ABLASTR library. Re-use from there.
    
    Co-authored-by: Remi Lehe <rlehe@lbl.gov>

--- src/particles/ImpactXParticleContainer.cpp
+++ src/particles/ImpactXParticleContainer.cpp
@@ -1,10 +1,12 @@
-/* Copyright 2021 Axel Huebl
+/* Copyright 2021 Axel Huebl, Remi Lehe
  *
  * This file is part of ImpactX.
  *
  * License: BSD-3-Clause-LBNL
  */
 #include "ImpactXParticleContainer.H"
+
+#include <ablastr/particles/ParticleMoments.H>
 
 #include <AMReX_AmrCore.H>
 #include <AMReX_AmrParGDB.H>
@@ -89,36 +91,17 @@ namespace impactx
             amrex::ParticleReal, amrex::ParticleReal>
     ImpactXParticleContainer::MinAndMaxPositions ()
     {
-        using PType = ImpactXParticleContainer::SuperParticleType;
-
-        // Get min and max for the local rank
-        amrex::ReduceOps<amrex::ReduceOpMin, amrex::ReduceOpMin, amrex::ReduceOpMin, amrex::ReduceOpMax, amrex::ReduceOpMax, amrex::ReduceOpMax> reduce_ops;
-        auto r = amrex::ParticleReduce<amrex::ReduceData<amrex::ParticleReal, amrex::ParticleReal, amrex::ParticleReal, amrex::ParticleReal, amrex::ParticleReal, amrex::ParticleReal>>(
-            *this,
-            [=] AMREX_GPU_DEVICE(const PType& p) noexcept -> amrex::GpuTuple<amrex::ParticleReal, amrex::ParticleReal, amrex::ParticleReal, amrex::ParticleReal, amrex::ParticleReal, amrex::ParticleReal>
-            {
-                amrex::ParticleReal x = p.pos(0);
-                amrex::ParticleReal y = p.pos(1);
-                amrex::ParticleReal z = p.pos(2);
-                return {x, y, z, x, y, z};
-            },
-            reduce_ops);
-
-        // Get min and max across all ranks
-        std::vector<amrex::ParticleReal> xyz_min = {
-            amrex::get<0>(r),
-            amrex::get<1>(r),
-            amrex::get<2>(r)
-        };
-        amrex::ParallelDescriptor::ReduceRealMin(xyz_min.data(), xyz_min.size());
-        std::vector<amrex::ParticleReal> xyz_max = {
-            amrex::get<3>(r),
-            amrex::get<4>(r),
-            amrex::get<5>(r)
-        };
-        amrex::ParallelDescriptor::ReduceRealMax(xyz_max.data(), xyz_max.size());
-
-        return {xyz_min[0], xyz_min[1], xyz_min[2], xyz_max[0], xyz_max[1], xyz_max[2]};
+        return ablastr::particles::MinAndMaxPositions(*this);
     }
 
+    std::tuple<
+            amrex::ParticleReal, amrex::ParticleReal,
+            amrex::ParticleReal, amrex::ParticleReal,
+            amrex::ParticleReal, amrex::ParticleReal>
+    ImpactXParticleContainer::MeanAndStdPositions ()
+    {
+        return ablastr::particles::MeanAndStdPositions<
+            ImpactXParticleContainer, RealSoA::w
+        >(*this);
+    }
 } // namespace impactx
