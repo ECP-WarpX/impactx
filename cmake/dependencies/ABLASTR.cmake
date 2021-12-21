66a00d012578bb90dcd3fca2f123f62737eba08d
Axel Huebl <axel.huebl@plasma.ninja>
2021-12-20 16:03:21 -0800

    Min/Max/Std Position: from ABLASTR (#33)
    * Min/Max/Std Position: from ABLASTR
    
    Move general min/max/std functions for particle containers into the
    ABLASTR library. Re-use from there.
    
    Co-authored-by: Remi Lehe <rlehe@lbl.gov>
    
    * CMake: Update ABLASTR commit
    
    Co-authored-by: Remi Lehe <rlehe@lbl.gov>

--- cmake/dependencies/ABLASTR.cmake
+++ cmake/dependencies/ABLASTR.cmake
@@ -102,7 +102,7 @@ set(ImpactX_ablastr_src ""
 set(ImpactX_ablastr_repo "https://github.com/ECP-WarpX/WarpX.git"
     CACHE STRING
     "Repository URI to pull and build ABLASTR from if(ImpactX_ablastr_internal)")
-set(ImpactX_ablastr_branch "7cd660b5061dd76240d8377fed3d2466aeaa346d"
+set(ImpactX_ablastr_branch "2a24ba13c214b3e4345529deecf627d6d113004e"
     CACHE STRING
     "Repository branch for ImpactX_ablastr_repo if(ImpactX_ablastr_internal)")
 
