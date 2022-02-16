b5d5998160c621d4e545e60030e51b0d7fe3ea7c
Chad Mitchell <ChadMitchell@lbl.gov>
2022-02-16 15:27:37 -0800

    Modified print loop in Diagnostics.cpp to copy data from GPU -> CPU for proper print to file.

--- src/particles/diagnostics/DiagnosticOutput.cpp
+++ src/particles/diagnostics/DiagnosticOutput.cpp
@@ -17,15 +17,23 @@ namespace diagnostics {
                                   OutputType const &otype) {
         using namespace amrex::literals; // for _rt and _prt
 
+        // create a host-side particle buffer
+        auto tmp = pc.make_alike<amrex::PinnedArenaAllocator>();
+
+        // copy device-to-host
+        bool const local = true;
+        tmp.copyParticles(pc, local);
+
         // loop over refinement levels
-        int const nLevel = pc.maxLevel();
-        for (int lev = 0; lev < nLevel; ++lev) {
+        int const nLevel = tmp.maxLevel() + 1;
+        for (int lev = 0; lev < nLevel; ++lev)
+        {
             // loop over all particle boxes
-            using ParIt = ImpactXParticleContainer::iterator;
-            for (ParIt pti(pc, lev); pti.isValid(); ++pti) {
+            using ParIt = typename decltype(tmp)::ParIterType; // you can even try to use ::ParConstIterType
+            for (ParIt pti(tmp, lev); pti.isValid(); ++pti) {
                 const int np = pti.numParticles();
 
-                // preparing access to particle data: AoS
+               // preparing access to particle data: AoS
                 using PType = ImpactXParticleContainer::ParticleType;
                 auto &aos = pti.GetArrayOfStructs();
                 PType *AMREX_RESTRICT aos_ptr = aos().dataPtr();
@@ -36,10 +44,21 @@ namespace diagnostics {
                 amrex::ParticleReal *const AMREX_RESTRICT part_py = soa_real[RealSoA::uy].dataPtr();
                 amrex::ParticleReal *const AMREX_RESTRICT part_pt = soa_real[RealSoA::pt].dataPtr();
 
+
                 if( otype == OutputType::PrintParticles) {
-                    amrex::ParallelFor(np, [=] AMREX_GPU_DEVICE(long i) {
+                // print out particles (this hack works only on CPU and on GPUs with
+                // unified memory access)
+                for (int i=0; i < np; ++i)
+                {
+
+                       // access AoS data such as positions and cpu/id
+//                       PType const& p = aos_ptr[i];
+//                       auto const id = p.id();
+//                       auto const cpu = p.cpu();
+//                       auto const pos = p.pos();
+
                         // access AoS data such as positions and cpu/id
-                        PType &p = aos_ptr[i];
+                        PType const& p = aos_ptr[i];
                         amrex::ParticleReal const x = p.pos(0);
                         amrex::ParticleReal const y = p.pos(1);
                         amrex::ParticleReal const t = p.pos(2);
@@ -54,10 +73,11 @@ namespace diagnostics {
                         amrex::PrintToFile("output_beam.txt") << t << " " << px << " ";
                         amrex::PrintToFile("output_beam.txt") << py << " " << pt << " " << std::endl;
 
-                    });
-                }
+                };
             } // end loop over all particle boxes
+         }
         } // env mesh-refinement level loop
+
     }
 } // namespace diagnostics
 } // namespace impactx
