730f429ce354105266c77f8e88567f0e2f7d0a37
Chad Mitchell <46825199+cemitch99@users.noreply.github.com>
2022-02-27 23:06:10 -0800

    Moved reference particle access inside "CoordinateTransformation". (#70)
    * Moved reference particle access inside "CoordinateTransformation".
    
    This removes hard-coded values for ptd, pzd that were previously used.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Minor Cleanup
    
    - missing include
    - remove leftover `Print()`
    - break overlong line
    - make math a bit more readable
    - simplify `RefPart` declaration and assignment
    - remove stray newline
    
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>

--- src/particles/transformation/CoordinateTransformation.cpp
+++ src/particles/transformation/CoordinateTransformation.cpp
@@ -12,14 +12,19 @@
 #include <AMReX_Extension.H>  // for AMREX_RESTRICT
 #include <AMReX_REAL.H>       // for ParticleReal
 
+#include <cmath>
+
 
 namespace impactx
 {
 namespace transformation {
     void CoordinateTransformation(ImpactXParticleContainer &pc,
-                                  Direction const &direction,
-                                  amrex::ParticleReal const pd) {
+                                  Direction const &direction) {
         using namespace amrex::literals; // for _rt and _prt
+
+        // preparing to access reference particle data: RefPart
+        RefPart ref_part = pc.GetRefParticle();
+        amrex::ParticleReal const pd = ref_part.pt;  // Design value of pt/mc2 = -gamma
 
         // loop over refinement levels
         int const nLevel = pc.maxLevel();
@@ -41,7 +46,9 @@ namespace transformation {
                 amrex::ParticleReal *const AMREX_RESTRICT part_pt = soa_real[RealSoA::pt].dataPtr();
 
                 if( direction == Direction::T2Z) {
-                    amrex::ParticleReal const pzd = pd;  // Design value of pz/mc = beta*gamma
+                    // Design value of pz/mc = beta*gamma
+                    amrex::ParticleReal const pzd = sqrt(pow(pd, 2) - 1.0);
+
                     T2Z t2z(pzd);
                     amrex::ParallelFor(np, [=] AMREX_GPU_DEVICE(long i) {
                         // access AoS data such as positions and cpu/id
