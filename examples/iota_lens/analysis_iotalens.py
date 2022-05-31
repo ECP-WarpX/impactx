476d489ad24ecdb6913848ff23d6c7a660c74544
Chad Mitchell <46825199+cemitch99@users.noreply.github.com>
2022-05-31 16:14:06 -0700

    Modified IOTA nonlinear lens example (#137)
    * Modified IOTA nonlinear lens example:
    
    1) use a sequence of thin nonlinear lenses + constant linear focusing
    2) modified input file to accept parameters for (H,I) diagnostics
    3) added as diagnostic output the functions (H,I), invariant
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update Formatting
    
    * Include Order (Alphabetical)
    
    * Analysis: Particle-Wise dH and dI comparison
    
    * Analysis: Join Table on particle ID first
    
    * Add documentation of input parameters used for diagnostic output of
    the IOTA invariants H and I.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>

---
+++ examples/iota_lens/analysis_iotalens.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+#
+# Copyright 2022 ImpactX contributors
+# Authors: Axel Huebl, Chad Mitchell
+# License: BSD-3-Clause-LBNL
+#
+
+import glob
+
+import numpy as np
+import pandas as pd
+from scipy.stats import moment
+
+
+def get_moments(beam):
+    """Calculate mean and std dev of functions defining the IOTA invariants
+
+    Returns
+    -------
+    meanH, sigH, meanI, sigI
+    """
+    meanH = np.mean(beam["H"])
+    sigH = moment(beam["H"],moment=2)**0.5
+    meanI = np.mean(beam["I"])
+    sigI = moment(beam["I"],moment=2)**0.5
+
+    return (
+        meanH, sigH, meanI, sigI)
+
+
+def read_all_files(file_pattern):
+    """Read in all CSV files from each MPI rank (and potentially OpenMP
+    thread). Concatenate into one Pandas dataframe.
+
+    Returns
+    -------
+    pandas.DataFrame
+    """
+    return pd.concat(
+        (
+            pd.read_csv(filename, delimiter=r"\s+")
+            for filename in glob.glob(file_pattern)
+        ),
+        axis=0,
+        ignore_index=True,
+    ).set_index('id')
+
+
+# initial/final beam on rank zero
+initial = read_all_files("diags/initial_nonlinear_lens_invariants.txt.*")
+final = read_all_files("diags/output_nonlinear_lens_invariants.txt.*")
+
+# compare number of particles
+num_particles = 10000
+assert num_particles == len(initial)
+assert num_particles == len(final)
+
+print("Initial Beam:")
+meanH, sigH, \
+   meanI, sigI = get_moments(initial)
+print(f"  meanH={meanH:e} sigH={sigH:e} meanI={meanI:e} sigI={sigI:e}")
+
+atol = 1.0  # a big number
+rtol = num_particles**-0.5  # from random sampling of a smooth distribution
+print(f"  rtol={rtol} (ignored: atol~={atol})")
+
+assert np.allclose(
+    [meanH, sigH, meanI, sigI],
+    [1.604948e-01, 1.757985e-01, 2.882956e-01, 3.844099e-01],
+    rtol=rtol,
+    atol=atol
+)
+
+
+print("")
+print("Final Beam:")
+meanH, sigH, \
+   meanI, sigI = get_moments(final)
+print(f"  meanH={meanH:e} sigH={sigH:e} meanI={meanI:e} sigI={sigI:e}")
+
+atol = 1.0  # a big number
+rtol = num_particles**-0.5  # from random sampling of a smooth distribution
+print(f"  rtol={rtol} (ignored: atol~={atol})")
+
+assert np.allclose(
+    [meanH, sigH, meanI, sigI],
+    [1.605336e-01, 1.756235e-01, 2.880581e-01, 3.839625e-01],
+    rtol=rtol,
+    atol=atol
+)
+
+# join tables on particle ID, so we can compare the same particle initial->final
+beam_joined = final.join(
+    initial,
+    lsuffix='_final',
+    rsuffix='_initial'
+)
+# add new columns: dH and dI
+beam_joined['dH'] = beam_joined["H_initial"] - beam_joined["H_final"]
+beam_joined['dI'] = beam_joined["I_initial"] - beam_joined["I_final"]
+#print(beam_joined)
+
+# particle-wise comparison of H & I initial to final
+atol = 0.076
+rtol = 1.0  # large number
+print()
+print(f"  atol={atol} (ignored: rtol~={rtol})")
+
+print(f"  dH_min={beam_joined['dH'].min()}, dH_max={beam_joined['dH'].max()}")
+assert np.allclose(beam_joined['dH'], 0.0, rtol=rtol, atol=atol)
+
+atol = 0.151
+print(f"  atol={atol} (ignored: rtol~={rtol})")
+print(f"  dI_min={beam_joined['dI'].min()}, dI_max={beam_joined['dI'].max()}")
+assert np.allclose(beam_joined['dI'], 0.0, rtol=rtol, atol=atol)
