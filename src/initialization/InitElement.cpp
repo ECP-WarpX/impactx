159bc5ebfbf7c2f251348f2519ad7daa8d7c0a18
Axel Huebl <axel.huebl@plasma.ninja>
2022-06-02 09:15:35 -0700

    Update License Texts

--- src/initialization/InitElement.cpp
+++ src/initialization/InitElement.cpp
@@ -1,7 +1,10 @@
-/* Copyright 2021-2022 Axel Huebl, Chad Mitchell, Ji Qiang
+/* Copyright 2022 The Regents of the University of California, through Lawrence
+ *           Berkeley National Laboratory (subject to receipt of any required
+ *           approvals from the U.S. Dept. of Energy). All rights reserved.
  *
  * This file is part of ImpactX.
  *
+ * Authors: Axel Huebl, Chad Mitchell, Ji Qiang
  * License: BSD-3-Clause-LBNL
  */
 #include "ImpactX.H"
--- src/particles/ChargeDeposition.cpp
+++ src/particles/ChargeDeposition.cpp
@@ -1,7 +1,10 @@
-/* Copyright 2022 Axel Huebl
+/* Copyright 2022 The Regents of the University of California, through Lawrence
+ *           Berkeley National Laboratory (subject to receipt of any required
+ *           approvals from the U.S. Dept. of Energy). All rights reserved.
  *
  * This file is part of ImpactX.
  *
+ * Authors: Axel Huebl
  * License: BSD-3-Clause-LBNL
  */
 #include "ImpactXParticleContainer.H"
