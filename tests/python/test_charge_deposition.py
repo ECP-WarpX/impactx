9974e43c26e326aee40bbba16f3ea14bdbf14aac
Axel Huebl <axel.huebl@plasma.ninja>
2022-09-09 02:21:56 -0700

    Simple Tests

--- tests/python/test_charge_deposition.py
+++ tests/python/test_charge_deposition.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+#
+# Copyright 2022 The ImpactX Community
+#
+# Authors: Axel Huebl
+# License: BSD-3-Clause-LBNL
+#
 # -*- coding: utf-8 -*-
 
 
@@ -14,11 +21,22 @@ def test_charge_deposition(save_png=True):
     """
     Deposit charge and access/plot it
     """
+    pp_amr = amrex.ParmParse("amr")
+    pp_amr.addarr("n_cell", [16, 24, 32])
+
     sim = impactx.ImpactX()
+
+    assert sim.n_cell == [16, 24, 32]
 
     sim.load_inputs_file("examples/fodo/input_fodo.in")
     sim.set_space_charge(True)
     sim.set_slice_step_diagnostics(False)
+
+    # Future:
+    # sim.ncell = [25, 25, 45]
+    # sim.domain = amrex.RealBox([1., 2., 3.], [4., 5., 6.])
+    print(f"sim.n_cell={sim.n_cell}")
+    print(f"sim.domain={sim.domain}")
 
     sim.init_grids()
     sim.init_beam_distribution_from_inputs()
@@ -36,6 +54,8 @@ def test_charge_deposition(save_png=True):
     beam_charge = dV * rs  # in C
     assert math.isclose(beam_charge, 1.0e-9)
 
+    half_z = sim.n_cell[2] // 2  # order: x,y,z
+
     f = plt.figure()
     ax = f.gca()
     ng = rho.nGrowVect
@@ -44,14 +64,21 @@ def test_charge_deposition(save_png=True):
         rbx = amrex.RealBox(bx, dr, gm.ProbLo())
 
         arr = rho.array(mfi)
-        arr_np = np.array(arr, copy=False)
+        arr_np = np.array(arr, copy=False)  # indices: comp, z, y, x
 
-        half_z = arr_np.shape[1] // 2  # indices: comp, z, y, x
+        # shift box to zero-based local mfi index space
+        half_z_local = half_z - bx.lo_vect[2]
+        bx.shift(bx.lo_vect * -1)
+        # check if the current tile contains the half-z plane
+        if half_z_local < 0 or half_z_local > arr_np.shape[2]:
+            continue
+
         comp = 0
         mu = 1.0e6  # m->mu
         im = ax.imshow(
             # arr_np[comp, half_z, ...] * dV,  # including guard
-            arr_np[comp, half_z, ng[1] : -ng[1], ng[0] : -ng[0]] * dV,  # w/o guard
+            arr_np[comp, half_z_local, ng[1] : -ng[1], ng[0] : -ng[0]]
+            * dV,  # w/o guard
             origin="lower",
             aspect="auto",
             extent=[rbx.lo(0) * mu, rbx.hi(0) * mu, rbx.lo(1) * mu, rbx.hi(1) * mu],
