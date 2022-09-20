ecccc14ed9b972df017c7b18f0dd85c581c26dbf
Andrew Myers <atmyers2@gmail.com>
2022-09-20 11:03:09 -0700

    enable tiling by default if not running on GPU

--- src/initialization/InitParser.cpp
+++ src/initialization/InitParser.cpp
@@ -22,5 +22,18 @@ namespace impactx::initialization
         bool abort_on_out_of_gpu_memory = true; // AMReX' default: false
         pp_amrex.query("abort_on_out_of_gpu_memory", abort_on_out_of_gpu_memory);
         pp_amrex.add("abort_on_out_of_gpu_memory", abort_on_out_of_gpu_memory);
+
+        // Here we override the default tiling option for particles, which is always
+        // "false" in AMReX, to "false" if compiling for GPU execution and "true"
+        // if compiling for CPU.
+        {
+            amrex::ParmParse pp_particles("particles");
+#ifdef AMREX_USE_GPU
+            bool do_tiling = false; // By default, tiling is off on GPU
+#else
+            bool do_tiling = true;
+#endif
+            pp_particles.queryAdd("do_tiling", do_tiling);
+        }
     }
 } // namespace impactx
