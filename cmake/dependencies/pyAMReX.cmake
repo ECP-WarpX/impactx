8e99a08e5deb7561ba59cf6c724ed09fe0226b43
Axel Huebl <axel.huebl@plasma.ninja>
2022-11-02 15:50:46 -0500

    Dependencies: 22.11
    Update ABLASTR, AMReX and pyAMReX

--- cmake/dependencies/pyAMReX.cmake
+++ cmake/dependencies/pyAMReX.cmake
@@ -76,7 +76,7 @@ option(ImpactX_pyamrex_internal "Download & build pyAMReX" ON)
 set(ImpactX_pyamrex_repo "https://github.com/AMReX-Codes/pyamrex.git"
     CACHE STRING
     "Repository URI to pull and build pyamrex from if(ImpactX_pyamrex_internal)")
-set(ImpactX_pyamrex_branch "development"
+set(ImpactX_pyamrex_branch "22.11"
     CACHE STRING
     "Repository branch for ImpactX_pyamrex_repo if(ImpactX_pyamrex_internal)")
 
