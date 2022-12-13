8ad39ab462b845d4be19bc6b4d9fd9c8debb59d8
Axel Huebl <axel.huebl@plasma.ninja>
2022-12-12 17:07:43 -0800

    Push: Move RefParticle in Constructor (#282)
    * Push: Move RefParticle in Constructor
    
    The RefPart is 88 bytes and should not be copied twice.
    
    * Move Element, too.

--- src/particles/Push.cpp
+++ src/particles/Push.cpp
@@ -13,6 +13,8 @@
 #include <AMReX_Extension.H>  // for AMREX_RESTRICT
 #include <AMReX_REAL.H>       // for ParticleReal
 
+#include <utility>
+
 
 namespace impactx
 {
@@ -51,9 +53,9 @@ namespace detail
                             amrex::ParticleReal* AMREX_RESTRICT part_py,
                             amrex::ParticleReal* AMREX_RESTRICT part_pt,
                             RefPart ref_part)
-            : m_element(element), m_aos_ptr(aos_ptr),
+            : m_element(std::move(element)), m_aos_ptr(aos_ptr),
               m_part_px(part_px), m_part_py(part_py), m_part_pt(part_pt),
-              m_ref_part(ref_part)
+              m_ref_part(std::move(ref_part))
         {
         }
 
