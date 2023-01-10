738f48c48b07cd5c868add934531fbf698358358
Axel Huebl <axel.huebl@plasma.ninja>
2023-01-10 14:05:08 -0800

    Python: Expose Finite/Thin Base Classes

--- src/python/elements.cpp
+++ src/python/elements.cpp
@@ -64,7 +64,31 @@ void init_elements(py::module& m)
         }, py::keep_alive<0, 1>()) /* Keep list alive while iterator is used */
     ;
 
-    py::class_<ConstF>(me, "ConstF")
+    // mixin classes
+
+    py::class_<elements::Finite>(me, "Finite")
+        .def(py::init<
+                 amrex::ParticleReal const,
+                 amrex::ParticleReal const
+             >(),
+             py::arg("ds"), py::arg("nslice") = 1,
+             "Mixin class for lattice elements with finite length."
+        )
+        .def_property_readonly("nslice", &elements::Finite::nslice)
+        .def_property_readonly("ds", &elements::Finite::ds)
+    ;
+
+    py::class_<elements::Thin>(me, "Thin")
+        .def(py::init<>(),
+             "Mixin class for lattice elements with zero length."
+        )
+        .def_property_readonly("nslice", &elements::Thin::nslice)
+        .def_property_readonly("ds", &elements::Thin::ds)
+    ;
+
+    // beam optics below
+
+    py::class_<ConstF, elements::Finite>(me, "ConstF")
         .def(py::init<
                 amrex::ParticleReal const,
                 amrex::ParticleReal const,
@@ -74,11 +98,9 @@ void init_elements(py::module& m)
              py::arg("ds"), py::arg("kx"), py::arg("ky"), py::arg("kt"), py::arg("nslice") = 1,
              "A linear Constant Focusing element."
         )
-        .def_property_readonly("nslice", &ConstF::nslice)
-        .def_property_readonly("ds", &ConstF::ds)
     ;
 
-    py::class_<DipEdge>(me, "DipEdge")
+    py::class_<DipEdge, elements::Thin>(me, "DipEdge")
         .def(py::init<
                 amrex::ParticleReal const,
                 amrex::ParticleReal const,
@@ -87,22 +109,18 @@ void init_elements(py::module& m)
              py::arg("psi"), py::arg("rc"), py::arg("g"), py::arg("K2"),
              "Edge focusing associated with bend entry or exit."
         )
-        .def_property_readonly("nslice", &DipEdge::nslice)
-        .def_property_readonly("ds", &DipEdge::ds)
     ;
 
-    py::class_<Drift>(me, "Drift")
+    py::class_<Drift, elements::Finite>(me, "Drift")
         .def(py::init<
                 amrex::ParticleReal const,
                 int const >(),
              py::arg("ds"), py::arg("nslice") = 1,
              "A drift."
         )
-        .def_property_readonly("nslice", &Drift::nslice)
-        .def_property_readonly("ds", &Drift::ds)
     ;
 
-    py::class_<Multipole>(me, "Multipole")
+    py::class_<Multipole, elements::Thin>(me, "Multipole")
         .def(py::init<
                 int const,
                 amrex::ParticleReal const,
@@ -110,30 +128,24 @@ void init_elements(py::module& m)
              py::arg("multiple"), py::arg("K_normal"), py::arg("K_skew"),
              "A general thin multipole element."
         )
-        .def_property_readonly("nslice", &Multipole::nslice)
-        .def_property_readonly("ds", &Multipole::ds)
     ;
 
-    py::class_<None>(me, "None")
+    py::class_<None, elements::Thin>(me, "None")
         .def(py::init<>(),
              "This element does nothing."
         )
-        .def_property_readonly("nslice", &None::nslice)
-        .def_property_readonly("ds", &None::ds)
     ;
 
-    py::class_<NonlinearLens>(me, "NonlinearLens")
+    py::class_<NonlinearLens, elements::Thin>(me, "NonlinearLens")
         .def(py::init<
                 amrex::ParticleReal const,
                 amrex::ParticleReal const>(),
              py::arg("knll"), py::arg("cnll"),
              "Single short segment of the nonlinear magnetic insert element."
         )
-        .def_property_readonly("nslice", &NonlinearLens::nslice)
-        .def_property_readonly("ds", &NonlinearLens::ds)
     ;
 
-    py::class_<Quad>(me, "Quad")
+    py::class_<Quad, elements::Finite>(me, "Quad")
         .def(py::init<
                 amrex::ParticleReal const,
                 amrex::ParticleReal const,
@@ -141,11 +153,9 @@ void init_elements(py::module& m)
              py::arg("ds"), py::arg("k"), py::arg("nslice") = 1,
              "A Quadrupole magnet."
         )
-        .def_property_readonly("nslice", &Quad::nslice)
-        .def_property_readonly("ds", &Quad::ds)
     ;
 
-    py::class_<RFCavity>(me, "RFCavity")
+    py::class_<RFCavity, elements::Finite>(me, "RFCavity")
         .def(py::init<
                 amrex::ParticleReal const,
                 amrex::ParticleReal const,
@@ -159,11 +169,9 @@ void init_elements(py::module& m)
              py::arg("mapsteps"), py::arg("nslice") = 1,
              "An RF cavity (with solenoid field)."
         )
-        .def_property_readonly("nslice", &RFCavity::nslice)
-        .def_property_readonly("ds", &RFCavity::ds)
     ;
 
-    py::class_<Sbend>(me, "Sbend")
+    py::class_<Sbend, elements::Finite>(me, "Sbend")
         .def(py::init<
                 amrex::ParticleReal const,
                 amrex::ParticleReal const,
@@ -171,18 +179,14 @@ void init_elements(py::module& m)
              py::arg("ds"), py::arg("rc"), py::arg("nslice") = 1,
              "An ideal sector bend."
         )
-        .def_property_readonly("nslice", &Sbend::nslice)
-        .def_property_readonly("ds", &Sbend::ds)
     ;
 
-    py::class_<ShortRF>(me, "ShortRF")
+    py::class_<ShortRF, elements::Thin>(me, "ShortRF")
         .def(py::init<
                 amrex::ParticleReal const,
                 amrex::ParticleReal const>(),
              py::arg("V"), py::arg("k"),
              "A short RF cavity element at zero crossing for bunching."
         )
-        .def_property_readonly("nslice", &ShortRF::nslice)
-        .def_property_readonly("ds", &ShortRF::ds)
     ;
 }
