5220ff37ff89a53c0fac6adad2ee2d52e3ac7380
Axel Huebl <axel.huebl@plasma.ninja>
2023-03-30 18:17:06 -0700

    CI: CMake-Easyinstall New Link
    New Download Link

--- .github/workflows/dependencies/clang-san-openmpi.sh
+++ .github/workflows/dependencies/clang-san-openmpi.sh
@@ -33,7 +33,7 @@ python3 -m pip install -r examples/requirements.txt
 
 # cmake-easyinstall
 #
-sudo curl -L -o /usr/local/bin/cmake-easyinstall https://git.io/JvLxY
+sudo curl -L -o /usr/local/bin/cmake-easyinstall https://raw.githubusercontent.com/ax3l/cmake-easyinstall/main/cmake-easyinstall
 sudo chmod a+x /usr/local/bin/cmake-easyinstall
 export CEI_SUDO="sudo"
 export CEI_TMP="/tmp/cei"
