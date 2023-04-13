30f93ec73ef30343d436ac51380bd38f928f8004
Axel Huebl <axel.huebl@plasma.ninja>
2023-04-13 16:14:28 -0700

    Python Tests: w/o Managed Memory (#340)
    * Python Tests: w/o Managed Memory
    
    * pyAMReX: dtoh copy functions
    
    * Limit D2H Copies to GPU runs

--- tests/python/test_charge_deposition.py
+++ tests/python/test_charge_deposition.py
@@ -56,14 +56,28 @@ def test_charge_deposition(save_png=True):
 
     half_z = sim.n_cell[2] // 2  # order: x,y,z
 
+    # for GPU runs, copy data from device to host for plotting
+    if impactx.Config.have_gpu:
+        rho_host = amrex.MultiFab(
+            rho.box_array(),
+            rho.dm(),
+            rho.n_comp(),
+            rho.n_grow_vect(),
+            amrex.MFInfo().set_arena(amrex.The_Pinned_Arena()),
+        )
+        amrex.dtoh_memcpy(rho_host, rho)
+    else:
+        rho_host = rho
+
+    # plot data slices
     f = plt.figure()
     ax = f.gca()
-    ng = rho.nGrowVect
-    for mfi in rho:
+    ng = rho_host.nGrowVect
+    for mfi in rho_host:
         bx = mfi.validbox()
         rbx = amrex.RealBox(bx, dr, gm.ProbLo())
 
-        arr = rho.array(mfi)  # TODO: explicit device-to-host memory copy
+        arr = rho_host.array(mfi)
         arr_np = np.array(arr, copy=False)  # indices: comp, z, y, x
 
         # shift box to zero-based local mfi index space
