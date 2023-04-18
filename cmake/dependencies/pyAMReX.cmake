786c4de0c8663fdc567412a10794ed4356405ed3
Axel Huebl <axel.huebl@plasma.ninja>
2023-04-17 19:24:23 -0700

    CMake: pyAMReX Update

--- cmake/dependencies/pyAMReX.cmake
+++ cmake/dependencies/pyAMReX.cmake
@@ -76,7 +76,7 @@ option(ImpactX_pyamrex_internal "Download & build pyAMReX" ON)
 set(ImpactX_pyamrex_repo "https://github.com/AMReX-Codes/pyamrex.git"
     CACHE STRING
     "Repository URI to pull and build pyamrex from if(ImpactX_pyamrex_internal)")
-set(ImpactX_pyamrex_branch "bc24ec54bbb982ce3c388b1caf5b9a0a573ce0a8"
+set(ImpactX_pyamrex_branch "a90fec615b314bb2d6143add9655cbf6afe61186"
     CACHE STRING
     "Repository branch for ImpactX_pyamrex_repo if(ImpactX_pyamrex_internal)")
 
