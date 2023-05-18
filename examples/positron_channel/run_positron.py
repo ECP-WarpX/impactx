53c03188d0a218b0b702211fd6287298f2c2d1d8
Chad Mitchell <ChadMitchell@lbl.gov>
2023-05-18 15:23:04 -0700

    Add positron input files.

---
+++ examples/positron_channel/run_positron.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+#
+# Copyright 2022-2023 ImpactX contributors
+# Authors: Axel Huebl, Chad Mitchell
+# License: BSD-3-Clause-LBNL
+#
+# -*- coding: utf-8 -*-
+
+import amrex
+from impactx import ImpactX, RefPart, distribution, elements
+
+sim = ImpactX()
+
+# set numerical parameters and IO control
+sim.particle_shape = 2  # B-spline order
+sim.space_charge = False
+# sim.diagnostics = False  # benchmarking
+sim.slice_step_diagnostics = True
+
+# domain decomposition & space charge mesh
+sim.init_grids()
+
+# load a 2 GeV electron beam with an initial
+# unnormalized rms emittance of 2 nm
+energy_MeV = 2.0e3  # reference energy
+bunch_charge_C = 1.0e-9  # used with space charge
+npart = 10000  # number of macro particles
+
+#   reference particle
+ref = sim.particle_container().ref_particle()
+ref.set_charge_qe(-1.0).set_mass_MeV(0.510998950).set_energy_MeV(energy_MeV)
+
+#   particle bunch
+distr = distribution.Waterbag(
+    sigmaX=3.9984884770e-5,
+    sigmaY=3.9984884770e-5,
+    sigmaT=1.0e-3,
+    sigmaPx=2.6623538760e-5,
+    sigmaPy=2.6623538760e-5,
+    sigmaPt=2.0e-3,
+    muxpx=-0.846574929020762,
+    muypy=0.846574929020762,
+    mutpt=0.0,
+)
+sim.add_particles(bunch_charge_C, distr, npart)
+
+# add beam diagnostics
+monitor = elements.BeamMonitor("monitor", backend="h5")
+
+# design the accelerator lattice)
+ns = 25  # number of slices per ds in the element
+fodo = [
+    monitor,
+    elements.Drift(ds=0.25, nslice=ns),
+    monitor,
+    elements.Quad(ds=1.0, k=1.0, nslice=ns),
+    monitor,
+    elements.Drift(ds=0.5, nslice=ns),
+    monitor,
+    elements.Quad(ds=1.0, k=-1.0, nslice=ns),
+    monitor,
+    elements.Drift(ds=0.25, nslice=ns),
+    monitor,
+]
+# assign a fodo segment
+sim.lattice.extend(fodo)
+
+# run simulation
+sim.evolve()
+
+# clean shutdown
+del sim
+amrex.finalize()
