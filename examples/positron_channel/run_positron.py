707fcac5b98442321f3ca5ef36115fdda33172bb
Chad Mitchell <ChadMitchell@lbl.gov>
2023-05-22 12:08:18 -0700

    Add Python equivalent example.

--- examples/positron_channel/run_positron.py
+++ examples/positron_channel/run_positron.py
@@ -22,25 +22,25 @@ sim.init_grids()
 
 # load a 2 GeV electron beam with an initial
 # unnormalized rms emittance of 2 nm
-energy_MeV = 2.0e3  # reference energy
-bunch_charge_C = 1.0e-9  # used with space charge
+energy_MeV = 10.0e3  # reference energy
+bunch_charge_C = 190.0e-12  # used with space charge
 npart = 10000  # number of macro particles
 
 #   reference particle
 ref = sim.particle_container().ref_particle()
-ref.set_charge_qe(-1.0).set_mass_MeV(0.510998950).set_energy_MeV(energy_MeV)
+ref.set_charge_qe(1.0).set_mass_MeV(0.510998950).set_energy_MeV(energy_MeV)
 
 #   particle bunch
-distr = distribution.Waterbag(
-    sigmaX=3.9984884770e-5,
-    sigmaY=3.9984884770e-5,
-    sigmaT=1.0e-3,
-    sigmaPx=2.6623538760e-5,
-    sigmaPy=2.6623538760e-5,
-    sigmaPt=2.0e-3,
-    muxpx=-0.846574929020762,
-    muypy=0.846574929020762,
-    mutpt=0.0,
+distr = distribution.Triangle(
+    sigmaX=5.028468056e-6,
+    sigmaY=4.976887423e-6,
+    sigmaT=8.4789873e-8,
+    sigmaPx=1.01616006e-7,
+    sigmaPy=1.026691578e-7,
+    sigmaPt=1.0e-3,
+    muxpx=-0.0,
+    muypy=0.0,
+    mutpt=0.999950003749688,
 )
 sim.add_particles(bunch_charge_C, distr, npart)
 
@@ -49,21 +49,20 @@ monitor = elements.BeamMonitor("monitor", backend="h5")
 
 # design the accelerator lattice)
 ns = 25  # number of slices per ds in the element
-fodo = [
+period = [
     monitor,
-    elements.Drift(ds=0.25, nslice=ns),
-    monitor,
-    elements.Quad(ds=1.0, k=1.0, nslice=ns),
-    monitor,
-    elements.Drift(ds=0.5, nslice=ns),
-    monitor,
-    elements.Quad(ds=1.0, k=-1.0, nslice=ns),
-    monitor,
-    elements.Drift(ds=0.25, nslice=ns),
+    elements.ChrQuad(ds=0.1, k=-6.674941, units=1, nslice=ns),
+    elements.ChrDrift(ds=0.3, nslice=ns),
+    elements.ChrQuad(ds=0.2, k=6.674941, units=1, nslice=ns),
+    elements.ChrDrift(ds=0.3, nslice=ns),
+    elements.ChrQuad(ds=0.1, k=-6.674941, units=1, nslice=ns),
+    elements.ChrDrift(ds=0.1, nslice=ns),
+    elements.ChrAcc(ds=1.8, ez=10871.950994502130424, bz=1.0e-3, nslice=ns),
+    elements.ChrDrift(ds=0.1, nslice=ns),
     monitor,
 ]
 # assign a fodo segment
-sim.lattice.extend(fodo)
+sim.lattice.extend(period)
 
 # run simulation
 sim.evolve()
