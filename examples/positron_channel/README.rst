a5e77d9ff4d364fee5aac23fc13935f73deeb905
Chad Mitchell <46825199+cemitch99@users.noreply.github.com>
2023-06-01 18:55:18 -0700

    Add positron LPA channel example (#362)
    * Examples for 3D space charge benchmarking
    
    - Modified the initial beam size in the IOTA lens benchmark example.
    - Added 2 benchmarks of 3D space charge for initial testing.
    - Add documentation for 2 benchmarks with space charge.
    - Add a benchmark example with space charge and periodic s-dependent focusing.
    - Added an s-dependent example using a Kurth beam without space charge.
    - Modified tolerance for IOTA lens benchmark example.
      Reduced tolerance to account for smaller initial beam size and
      improved preservation of invariants of motion.
    - Modified tolerances of space charge examples to allow CI tests to
      pass when space charge is not active.
    
    - Modified tolerance for space charge examples.
      These should fail unless space charge is turned on.
    
    * Update input_kurth_10nC.in
    
    Selected numerical values for amr.n_cell, lattice.nslice, and geometry.prob_relative.
    
    * Delete input_kurth_10nC.in
    
    This is not part of this pull request
    
    * Add positron input files.
    
    * Add Python equivalent example.
    
    * Modify Python example to support 250 periods.
    
    * Update example documentation.
    
    * Add example documentation to ToC in docs/source/usage/examples.rst.
    
    * Add repeat for 250 periods.
    
    * Add example to examples/CmakeLists.txt
    
    * Merge development and modify inputs.
    
    * Add analysis script.
    
    * Update analysis_positron.py
    
    Loosen tolerance slightly
    
    * Increase uncorrelated energy spread.
    
    * Update run_positron.py
    
    Correct Python input file inline documentation.
    
    * .rst Title Formatting

---
+++ examples/positron_channel/README.rst
@@ -0,0 +1,49 @@
+.. _examples-positron:
+
+Positron Channel
+================
+
+Acceleration of a positron beam with large (10%) energy spread, from 10 GeV
+to 2.5 TeV, for parameters based on possible staging of a laser-wakefield accelerator.
+
+The lattice consists of 250 periods, each consisting of a quadrupole triplet
+followed by 10 GeV energy gain in a uniform field.
+
+We use a 190 pC positron beam with initial normalized rms emittance of 10 nm,
+rms beam size of 5 microns, and a triangular current pulse with end-to-end pulse
+length of 0.12 ps.
+
+In this test, the initial and final values of :math:`\sigma_x`, :math:`\sigma_y`, :math:`\sigma_t`, :math:`\epsilon_x`, :math:`\epsilon_y`, and :math:`\epsilon_t` must agree with nominal values.
+
+
+Run
+---
+
+This example can be run as a Python script (``python3 run_positron.py``) or with an app with an input file (``impactx input_positron.in``).
+Each can also be prefixed with an `MPI executor <https://www.mpi-forum.org>`__, such as ``mpiexec -n 4 ...`` or ``srun -n 4 ...``, depending on the system.
+
+.. tab-set::
+
+   .. tab-item:: Python Script
+
+       .. literalinclude:: run_positron.py
+          :language: python3
+          :caption: You can copy this file from ``examples/positron_channel/run_positron.py``.
+
+   .. tab-item:: App Input File
+
+       .. literalinclude:: input_positron.in
+          :language: ini
+          :caption: You can copy this file from ``examples/positron_channel/input_positron.in``.
+
+
+Analyze
+-------
+
+We run the following script to analyze correctness:
+
+.. dropdown:: Script ``analysis_positron.py``
+
+   .. literalinclude:: analysis_positron.py
+      :language: python3
+      :caption: You can copy this file from ``examples/positron_channel/analysis_positron.py``.
---
+++ examples/positron_channel/run_positron.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+#
+# Copyright 2022-2023 ImpactX contributors
+# Authors: Axel Huebl, Chad Mitchell
+# License: BSD-3-Clause-LBNL
+#
+# -*- coding: utf-8 -*-
+
+import amrex.space3d as amr
+from impactx import ImpactX, RefPart, distribution, elements
+
+sim = ImpactX()
+
+# set numerical parameters and IO control
+sim.particle_shape = 2  # B-spline order
+sim.space_charge = False
+# sim.diagnostics = False  # benchmarking
+sim.slice_step_diagnostics = False
+
+# domain decomposition & space charge mesh
+sim.init_grids()
+
+# load a 10 GeV positron beam with an initial
+# normalized rms emittance of 10 nm
+energy_MeV = 10.0e3  # reference energy
+bunch_charge_C = 190.0e-12  # used with space charge
+npart = 10000  # number of macro particles
+
+#   reference particle
+ref = sim.particle_container().ref_particle()
+ref.set_charge_qe(1.0).set_mass_MeV(0.510998950).set_energy_MeV(energy_MeV)
+
+#   particle bunch
+distr = distribution.Triangle(
+    sigmaX=5.054566450e-6,
+    sigmaY=5.054566450e-6,
+    sigmaT=8.43732950e-7,
+    sigmaPx=1.01091329e-7,
+    sigmaPy=1.01091329e-7,
+    sigmaPt=1.0e-2,
+    muxpx=0.0,
+    muypy=0.0,
+    mutpt=0.995037190209989,
+)
+sim.add_particles(bunch_charge_C, distr, npart)
+
+# add beam diagnostics
+monitor = elements.BeamMonitor("monitor", backend="h5")
+
+# design the accelerator lattice)
+ns = 1  # number of slices per ds in the element
+period = [
+    monitor,
+    elements.ChrQuad(ds=0.1, k=-6.674941, units=1, nslice=ns),
+    elements.ChrDrift(ds=0.3, nslice=ns),
+    elements.ChrQuad(ds=0.2, k=6.674941, units=1, nslice=ns),
+    elements.ChrDrift(ds=0.3, nslice=ns),
+    elements.ChrQuad(ds=0.1, k=-6.674941, units=1, nslice=ns),
+    elements.ChrDrift(ds=0.1, nslice=ns),
+    elements.ChrAcc(ds=1.8, ez=10871.950994502130424, bz=1.0e-12, nslice=ns),
+    elements.ChrDrift(ds=0.1, nslice=ns),
+    monitor,
+]
+
+num_periods = 250
+
+lattice = period * num_periods
+
+sim.lattice.extend(lattice)
+
+# run simulation
+sim.evolve()
+
+# clean shutdown
+del sim
+amr.finalize()
