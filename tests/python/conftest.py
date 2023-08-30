4a52d2f8cd17d3af027589a1b86daf20d9895894
Axel Huebl <axel.huebl@plasma.ninja>
2023-08-29 22:30:49 -0700

    pytest: Temporary Working Directory (#424)
    * Fix: pytest Wait 1 Sec
    
    Simetimes our tests are so quick that they
    finish below 1second. That does not work with
    `UniqueStrings` in AMReX' `UtilCreateCleanDirectory`,
    which can only do one name per second:
    https://github.com/AMReX-Codes/amrex/blob/23.08/Src/Base/AMReX_Utility.cpp#L186-L199
    
    If the target exist, the behavior of `std::rename` is
    implementation dependent. We see crashes of this
    on macOS.
    https://en.cppreference.com/w/cpp/io/c/rename
    
    * Use Temporary Working Directory
    
    Avoid clean-up needs by using a cwd that is unique per test.
    
    * All the yielded results in the cwd context
    
    * Set CWD of runner

--- tests/python/conftest.py
+++ tests/python/conftest.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 import pytest
 
 import amrex.space3d as amr
@@ -12,23 +14,27 @@ if impactx.Config.have_mpi:
 else:
     print("NO mpi4py load")
 
+# base path for input files
+basepath = os.getcwd()
+
 
 @pytest.fixture(autouse=True, scope="function")
-def amrex_init():
-    amr.initialize(
-        [
-            # print AMReX status messages
-            "amrex.verbose=2",
-            # throw exceptions and create core dumps instead of
-            # AMReX backtrace files: allows to attach to
-            # debuggers
-            "amrex.throw_exception=1",
-            "amrex.signal_handling=0",
-            # abort GPU runs if out-of-memory instead of swapping to host RAM
-            "amrex.abort_on_out_of_gpu_memory=1",
-            # do not rely on implicit host-device memory transfers
-            "amrex.the_arena_is_managed=0",
-        ]
-    )
-    yield
-    amr.finalize()
+def amrex_init(tmpdir):
+    with tmpdir.as_cwd():
+        amr.initialize(
+            [
+                # print AMReX status messages
+                "amrex.verbose=2",
+                # throw exceptions and create core dumps instead of
+                # AMReX backtrace files: allows to attach to
+                # debuggers
+                "amrex.throw_exception=1",
+                "amrex.signal_handling=0",
+                # abort GPU runs if out-of-memory instead of swapping to host RAM
+                "amrex.abort_on_out_of_gpu_memory=1",
+                # do not rely on implicit host-device memory transfers
+                "amrex.the_arena_is_managed=0",
+            ]
+        )
+        yield
+        amr.finalize()
