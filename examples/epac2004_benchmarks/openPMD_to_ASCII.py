fc49be291f1ad8406ab6ca92f8e07630fe354115
Chad Mitchell <46825199+cemitch99@users.noreply.github.com>
2023-09-22 12:59:43 -0700

    Update openPMD_to_ASCII.py

--- examples/epac2004_benchmarks/openPMD_to_ASCII.py
+++ examples/epac2004_benchmarks/openPMD_to_ASCII.py
@@ -1,9 +1,6 @@
 import openpmd_api as io  # install with python3 -m pip install openpmd-api
 
-# open the data series
-series = io.Series("diags/openPMD/monitor.h5", io.Access.read_only)
-
-# initial/final beam
+# access the initial/final beam
 series = io.Series("diags/openPMD/monitor.h5", io.Access.read_only)
 last_step = list(series.iterations)[-1]
 initial = series.iterations[1].particles["beam"].to_df()
@@ -12,5 +9,5 @@ final = series.iterations[last_step].particles["beam"].to_df()
 # print the intial beam
 initial.to_csv("beam_initial.csv", sep=" ", header=True)
 
-# select the last time the beam passed the beam monitor element
+# print the final beam
 final.to_csv("beam_final.csv", sep=" ", header=True)
