f6d275e62a51f057ac647d3cb26fe5b8671d66ba
Axel Huebl <axel.huebl@plasma.ninja>
2023-09-26 21:40:52 -0700

    Use latest pyAMReX

--- cmake/dependencies/pyAMReX.cmake
+++ cmake/dependencies/pyAMReX.cmake
@@ -79,7 +79,7 @@ option(ImpactX_pyamrex_internal "Download & build pyAMReX" ON)
 set(ImpactX_pyamrex_repo "https://github.com/AMReX-Codes/pyamrex.git"
     CACHE STRING
     "Repository URI to pull and build pyamrex from if(ImpactX_pyamrex_internal)")
-set(ImpactX_pyamrex_branch "23.08"
+set(ImpactX_pyamrex_branch "development"
     CACHE STRING
     "Repository branch for ImpactX_pyamrex_repo if(ImpactX_pyamrex_internal)")
 
