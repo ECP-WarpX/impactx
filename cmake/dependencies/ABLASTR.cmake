9709128359e604953ac4cf836e27ab5f9287750a
Axel Huebl <axel.huebl@plasma.ninja>
2023-12-04 21:10:28 -0800

    Release 23.12 (#478)
    Prepare the December release of ImpactX.

--- cmake/dependencies/ABLASTR.cmake
+++ cmake/dependencies/ABLASTR.cmake
@@ -144,7 +144,7 @@ macro(find_ablastr)
         set(COMPONENT_DIM 3D)
         set(COMPONENT_PRECISION ${ImpactX_PRECISION} P${ImpactX_PRECISION})
 
-        find_package(ABLASTR 23.11 CONFIG REQUIRED COMPONENTS ${COMPONENT_DIM})
+        find_package(ABLASTR 23.12 CONFIG REQUIRED COMPONENTS ${COMPONENT_DIM})
         message(STATUS "ABLASTR: Found version '${ABLASTR_VERSION}'")
     endif()
 
@@ -178,7 +178,7 @@ set(ImpactX_openpmd_src ""
 set(ImpactX_ablastr_repo "https://github.com/ECP-WarpX/WarpX.git"
     CACHE STRING
     "Repository URI to pull and build ABLASTR from if(ImpactX_ablastr_internal)")
-set(ImpactX_ablastr_branch "6eedf2cdb2ffcaf2d9bb26139e3671b3bcca73ec"
+set(ImpactX_ablastr_branch "23.12"
     CACHE STRING
     "Repository branch for ImpactX_ablastr_repo if(ImpactX_ablastr_internal)")
 
@@ -186,7 +186,7 @@ set(ImpactX_ablastr_branch "6eedf2cdb2ffcaf2d9bb26139e3671b3bcca73ec"
 set(ImpactX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(ImpactX_amrex_internal)")
-set(ImpactX_amrex_branch "9b733ec45cd93a80234a7c98248b6eb4816589d5"
+set(ImpactX_amrex_branch ""
     CACHE STRING
     "Repository branch for ImpactX_amrex_repo if(ImpactX_amrex_internal)")
 
