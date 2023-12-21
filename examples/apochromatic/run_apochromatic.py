369817a42d78289aa26622a5a70b7098ae219031
Chad Mitchell <46825199+cemitch99@users.noreply.github.com>
2023-12-21 13:19:37 -0800

    Update run_apochromatic.py
    Update Python to 10^5 particles

--- examples/apochromatic/run_apochromatic.py
+++ examples/apochromatic/run_apochromatic.py
@@ -24,7 +24,7 @@ sim.init_grids()
 # unnormalized rms emittance of 2 nm
 kin_energy_MeV = 100.0e3  # reference energy
 bunch_charge_C = 1.0e-9  # used with space charge
-npart = 10000  # number of macro particles
+npart = 100000  # number of macro particles
 
 #   reference particle
 ref = sim.particle_container().ref_particle()
