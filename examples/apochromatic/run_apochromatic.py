02b2f8ead7279690c455c59879cbb2feec368096
Chad Mitchell <ChadMitchell@lbl.gov>
2023-12-21 11:48:35 -0800

    Fix merge

--- examples/apochromatic/run_apochromatic.py
+++ examples/apochromatic/run_apochromatic.py
@@ -31,7 +31,7 @@ ref = sim.particle_container().ref_particle()
 ref.set_charge_qe(-1.0).set_mass_MeV(0.510998950).set_kin_energy_MeV(kin_energy_MeV)
 
 #   particle bunch
-distr = distribution.Gaussian(
+distr = distribution.Waterbag(
     sigmaX=1.288697604e-6,
     sigmaY=1.288697604e-6,
     sigmaT=1.0e-6,
