5f798c29692707f270ebe7d4656303db0909f8db
Axel Huebl <axel.huebl@plasma.ninja>
2024-01-06 12:58:19 -0800

    Release 24.01
    Pepare the January release of ImpactX.

--- setup.py
+++ setup.py
@@ -235,7 +235,7 @@ with open("./requirements.txt") as f:
 setup(
     name="impactx",
     # note PEP-440 syntax: x.y.zaN but x.y.z.devN
-    version="23.12",
+    version="24.01",
     packages=["impactx"],
     # Python sources:
     package_dir={"": "src/python"},
