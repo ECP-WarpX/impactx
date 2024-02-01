b1e0cfe844e19da85d0264862f8ef3c059e37580
Chad Mitchell <ChadMitchell@lbl.gov>
2024-02-01 11:52:08 -0800

    Add chromatic plasma lens model.

--- src/initialization/InitElement.cpp
+++ src/initialization/InitElement.cpp
@@ -294,6 +294,15 @@ namespace detail
             pp_element.queryAdd("units", units);
 
             m_lattice.emplace_back( ChrQuad(ds, k, units, a["dx"], a["dy"], a["rotation_degree"], nslice) );
+        } else if (element_type == "plasma_lens_chromatic")
+        {
+            auto a = detail::query_alignment(pp_element);
+            auto const [ds, nslice] = detail::query_ds(pp_element, nslice_default);
+
+            amrex::ParticleReal g;
+            pp_element.get("g", g);
+
+            m_lattice.emplace_back( ChrPlasmaLens(ds, g, a["dx"], a["dy"], a["rotation_degree"], nslice) );
         } else if (element_type == "drift_exact")
         {
             auto const [ds, nslice] = detail::query_ds(pp_element, nslice_default);
