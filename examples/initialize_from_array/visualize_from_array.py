9b0e65453a20ae25a42e4ce1784118dd4ea3f42b
RTSandberg <rsandberg@lbl.gov>
2024-03-19 16:28:02 -0700

    review suggestions

--- examples/initialize_from_array/visualize_from_array.py
+++ examples/initialize_from_array/visualize_from_array.py
@@ -23,7 +23,7 @@ micron = 1.0e6
 step = 1
 beam_at_step = beam_impactx_surrogate_series.iterations[step].particles["beam"].to_df()
 ref_part_step = impactx_surrogate_ref_particle.loc[step]
-ref_part = myref = coord.MyRefPart(
+ref_part = coord.MyRefPart(
     ref_part_step["x"],
     ref_part_step["y"],
     ref_part_step["z"],
@@ -53,38 +53,11 @@ ax = axT[0]
 ax.hist2d(x * millimeter, y * millimeter, bins=200)
 ax.set_xlabel("x (mm)")
 ax.set_ylabel("y (mm)")
-# ax = axT[0][1]
-# ax.hist2d(z*millimeter,x*millimeter,bins=200);
-# ax.set_xlabel('z (mm)')
-# ax.set_ylabel('x (mm)')
-# ax = axT[0][2]
-# ax.hist2d(z*millimeter,y*millimeter,bins=200);
-# ax.set_xlabel('z (mm)')
-# ax.set_ylabel('y (mm)')
 
 ax = axT[1]
 ax.hist2d(px, py, bins=200)
 ax.set_xlabel("px")
 ax.set_ylabel("py")
-# ax = axT[1][1]
-# ax.hist2d(pz,px,bins=200);
-# ax.set_xlabel('pz')
-# ax.set_ylabel('px')
-# ax.ticklabel_format(axis='x', style='sci', scilimits=(-1,1))
-# ax = axT[1][2]
-# ax.hist2d(pz,py,bins=200);
-# ax.set_xlabel('pz')
-# ax.set_ylabel('py')
-# ax.ticklabel_format(axis='x', style='sci', scilimits=(-1,1))
-
-# ax = axT[2][0]
-# ax.hist2d(x*millimeter,px,bins=200);
-# ax.set_xlabel('x (mm)')
-# ax.set_ylabel('px')
-# ax = axT[2][1]
-# ax.hist2d(y*millimeter,py,bins=200);
-# ax.set_xlabel('y (mm)')
-# ax.set_ylabel('py')
 ax = axT[2]
 ax.hist2d(z * millimeter, pz, bins=200)
 ax.set_xlabel("z (mm)")
