93e3a987da01965705a55f2a0f242fdc0d453bdd
Axel Huebl <axel.huebl@plasma.ninja>
2024-03-26 15:36:23 -0700

    Diagnostics: NLL Invariants to Monitor
    The calculation of the invariants of motion H and I for the
    nonlinear lens were still calculated in a slow, inefficient way using
    per-particle ASCII output.
    
    This moves the calculation and output to the beam monitor, where
    it can be optionally enabled and has significantly more performant
    I/O throughput using openPMD.

---
+++ src/particles/elements/diagnostics/AdditionalProperties.cpp
@@ -0,0 +1,102 @@
+/* Copyright 2022-2023 The Regents of the University of California, through Lawrence
+ *           Berkeley National Laboratory (subject to receipt of any required
+ *           approvals from the U.S. Dept. of Energy). All rights reserved.
+ *
+ * This file is part of ImpactX.
+ *
+ * Authors: Axel Huebl
+ * License: BSD-3-Clause-LBNL
+ */
+
+#include "openPMD.H"
+#include "particles/diagnostics/NonlinearLensInvariants.H"
+#include "particles/ImpactXParticleContainer.H"
+
+#include <AMReX.H>
+#include <AMReX_BLProfiler.H>
+#include <AMReX_REAL.H>
+#include <AMReX_RealVect.H>
+#include <AMReX_ParmParse.H>
+
+#include <string>
+#include <utility>
+
+
+namespace impactx::diagnostics
+{
+    void
+    add_optional_properties (
+        std::string const & element_name,
+        ImpactXParticleContainer & pc
+    )
+    {
+        // Parse the diagnostic parameters
+        amrex::ParmParse pp_element(element_name);
+
+        bool enabled = false;
+        pp_element.queryAdd("nonlinear_lens_invariants", enabled);
+        if (!enabled)
+            return;
+
+        amrex::ParticleReal alpha = 0.0;
+        pp_element.queryAdd("alpha", alpha);
+
+        amrex::ParticleReal beta = 1.0;
+        pp_element.queryAdd("beta", beta);
+
+        amrex::ParticleReal tn = 0.4;
+        pp_element.queryAdd("tn", tn);
+
+        amrex::ParticleReal cn = 0.01;
+        pp_element.queryAdd("cn", cn);
+
+        NonlinearLensInvariants const nonlinear_lens_invariants(alpha, beta, tn, cn);
+
+        // profile time spent here
+        std::string profile_name = "impactx::Push::" + std::string(BeamMonitor::name) + "::add_optional_properties";
+        BL_PROFILE(profile_name);
+
+        // add runtime properties for H and I
+        bool comm = false;
+        pc.AddRealComp("H", comm);
+        pc.AddRealComp("I", comm);
+
+        // loop over refinement levels
+        int const nLevel = pc.finestLevel();
+        for (int lev = 0; lev <= nLevel; ++lev) {
+            // loop over all particle boxes
+            using ParIt = ImpactXParticleContainer::iterator;
+            for (ParIt pti(pc, lev); pti.isValid(); ++pti) {
+                const int np = pti.numParticles();
+
+                // preparing access to particle data: SoA of Reals
+                auto & soa = pti.GetStructOfArrays();
+                auto const * part_x = soa.GetRealData(RealSoA::x).data();
+                auto const * part_y = soa.GetRealData(RealSoA::y).data();
+                auto const * part_px = soa.GetRealData(RealSoA::px).data();
+                auto const * part_py = soa.GetRealData(RealSoA::py).data();
+
+                const int num_real_comps = soa.NumRealComps();
+                auto * part_H = soa.GetRealData(num_real_comps - 2).data();
+                auto * part_I = soa.GetRealData(num_real_comps - 1).data();
+
+                amrex::ParallelFor(np, [=] AMREX_GPU_DEVICE (long i) {
+                    amrex::ParticleReal const x = part_x[i];
+                    amrex::ParticleReal const y = part_y[i];
+
+                    amrex::ParticleReal const px = part_px[i];
+                    amrex::ParticleReal const py = part_py[i];
+
+                    // calculate invariants of motion
+                    NonlinearLensInvariants::Data const HI_out =
+                        nonlinear_lens_invariants(x, y, px, py);
+
+                    // write particle invariant data
+                    part_H[i] = HI_out.H;
+                    part_I[i] = HI_out.I;
+                });
+            } // end loop over all particle boxes
+        } // end mesh-refinement level loop
+    }
+
+} // namespace impactx::diagnostics
