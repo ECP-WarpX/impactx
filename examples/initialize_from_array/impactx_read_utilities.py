bbe4b687d6edc39280d46440b3e2f5a195d7cc60
Ryan Sandberg <RSandberg@lbl.gov>
2024-04-08 14:56:03 -0700

    Feature: Initialize beam from arrays (#560)
    * start beam from array example
    * load from array, visualize, and test
    * include figure in documentation
    * clean up utility function names and run script
    * review suggestions
    * add test to CI, make sure it runs
    * update CI test folder
    * Monitor: Use HDF5 for CI
    * address review comments
    * Minor Updates to Docs & Strings
    
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>

---
+++ examples/initialize_from_array/impactx_read_utilities.py
@@ -0,0 +1,28 @@
+import glob
+import re
+
+import pandas as pd
+
+
+def read_file(file_pattern):
+    for filename in glob.glob(file_pattern):
+        df = pd.read_csv(filename, delimiter=r"\s+")
+        if "step" not in df.columns:
+            step = int(re.findall(r"[0-9]+", filename)[0])
+            df["step"] = step
+        yield df
+
+
+def read_time_series(file_pattern):
+    """Read in all CSV files from each MPI rank (and potentially OpenMP
+    thread). Concatenate into one Pandas dataframe.
+
+    Returns
+    -------
+    pandas.DataFrame
+    """
+    return pd.concat(
+        read_file(file_pattern),
+        axis=0,
+        ignore_index=True,
+    )  # .set_index('id')
