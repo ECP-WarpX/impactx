d9748abfd94b66d2e2a979c07c09a89326f726e3
Alex Bojanich <abojanich123@gmail.com>
2024-07-25 16:16:09 -0700

    Update test_charge_deposition.py

--- tests/python/test_charge_deposition.py
+++ tests/python/test_charge_deposition.py
@@ -24,8 +24,8 @@ def test_charge_deposition(save_png=True):
 
     sim.n_cell = [24, 32, 40]
     sim.load_inputs_file(
-        basepath + "/../../examples/chicane/input_chicane.in"
-    )  # examples/fodo/input_fodo.in
+        basepath + "/examples/chicane/input_chicane.in"
+    )  # /examples/fodo/input_fodo.in
     sim.slice_step_diagnostics = False
 
     # Future:
