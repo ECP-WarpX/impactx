b0b11aefade086a362247df58de7934b69c570e3
Alex Bojanich <abojanich123@gmail.com>
2024-08-07 13:17:51 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /* Copyright 2022-2023 The Regents of the University of California, through Lawrence
- * Berkeley National Laboratory (subject to receipt of any required
- * approvals from the U.S. Dept. of Energy). All rights reserved.
+ *           Berkeley National Laboratory (subject to receipt of any required
+ *           approvals from the U.S. Dept. of Energy). All rights reserved.
  *
  * This file is part of ImpactX.
  *
