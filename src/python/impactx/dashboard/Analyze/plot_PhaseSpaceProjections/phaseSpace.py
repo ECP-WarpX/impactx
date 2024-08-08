65ba6146f5dddba40ab1674ffd5bee0a1d0d389a
Parthib Roy <proy@lbl.gov>
2024-08-07 20:43:21 -0700

    removed unused code

--- src/python/impactx/dashboard/Analyze/plot_PhaseSpaceProjections/phaseSpace.py
+++ src/python/impactx/dashboard/Analyze/plot_PhaseSpaceProjections/phaseSpace.py
@@ -36,7 +36,7 @@ def fig_to_base64(fig):
     return base64.b64encode(buf.read()).decode("utf-8")
 
 
-def run_simulation(save_png=True):
+def run_simulation():
     """
     This tests using ImpactX and Pandas Dataframes
     """
@@ -57,32 +57,10 @@ def run_simulation(save_png=True):
     ref = pc.ref_particle()
     ref.set_charge_qe(-1.0).set_mass_MeV(0.510998950).set_kin_energy_MeV(kin_energy_MeV)
 
-    #   particle bunch
-    # distr = distribution.Waterbag(
-    #     lambdaX=3.9984884770e-5,
-    #     lambdaY=3.9984884770e-5,
-    #     lambdaT=1.0e-3,
-    #     lambdaPx=2.6623538760e-5,
-    #     lambdaPy=2.6623538760e-5,
-    #     lambdaPt=2.0e-3,
-    #     muxpx=-0.846574929020762,
-    #     muypy=0.846574929020762,
-    #     mutpt=0.0,
-    # )
     distr = save_distribution_parameters()
 
     sim.add_particles(bunch_charge_C, distr, npart)
 
-    assert pc.total_number_of_particles() == npart
-
-    # init accelerator lattice
-    # fodo = [
-    #     elements.Drift(0.25),
-    #     elements.Quad(1.0, 1.0),
-    #     elements.Drift(0.5),
-    #     elements.Quad(1.0, -1.0),
-    #     elements.Drift(0.25),
-    # ]
     fodo = save_lattice_elements()
 
     sim.lattice.extend(fodo)
@@ -90,24 +68,6 @@ def run_simulation(save_png=True):
     # simulate
     sim.evolve()
 
-    # check local particles
-    df = pc.to_df(local=True)
-    print(df)
-
-    # ensure the column heads are correctly labeled
-    assert df.columns.tolist() == [
-        "idcpu",
-        "position_x",
-        "position_y",
-        "position_t",
-        "momentum_x",
-        "momentum_y",
-        "momentum_t",
-        "qm",
-        "weighting",
-    ]
-
-    # fig = pc.plot_phasespace()
     fig = adjusted_settings_plot(pc)
     fig_original = pc.plot_phasespace()
 
