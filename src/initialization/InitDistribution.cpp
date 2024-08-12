3404f60923646a44df64efd60947fb486cd086c0
Alex Bojanich <abojanich@lbl.gov>
2024-08-12 11:33:26 -0700

    Remove global variable declaration for charge

--- src/initialization/InitDistribution.cpp
+++ src/initialization/InitDistribution.cpp
@@ -28,9 +28,6 @@
 #include <type_traits>
 #include <variant>
 
-//Global declaration
-amrex::ParticleReal bunch_charge = 0.0;
-
 namespace impactx
 {
 
@@ -221,7 +218,7 @@ namespace impactx
         amrex::ParticleReal kin_energy = 0.0;  // Beam kinetic energy (MeV)
         pp_dist.get("kin_energy", kin_energy);
 
-        //amrex::ParticleReal bunch_charge = 0.0;  // Bunch charge (C) - Define externally to make global variable
+        amrex::ParticleReal bunch_charge = 0.0;  // Bunch charge (C)
         pp_dist.get("charge", bunch_charge);
 
         std::string particle_type;  // Particle type
