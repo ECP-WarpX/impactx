cd1682e3538eb0a2da16849e5c7b04b6d9a9f199
ax3l <ax3l@users.noreply.github.com>
2024-08-12 07:06:12 +0000

    Update Stub Files

--- src/python/impactx/__init__.pyi
+++ src/python/impactx/__init__.pyi
@@ -36,7 +36,7 @@ from . import madx_to_impactx
 __all__ = ['Config', 'CoordSystem', 'ImpactX', 'ImpactXParConstIter', 'ImpactXParIter', 'ImpactXParticleContainer', 'MADXParser', 'RefPart', 'amr', 'coordinate_transformation', 'cxx', 'distribution', 'elements', 'extensions', 'impactx_pybind', 'madx_to_impactx', 'os', 'push', 'read_beam', 'read_lattice', 'register_ImpactXParIter_extension', 'register_ImpactXParticleContainer_extension', 's', 't']
 __author__: str = 'Axel Huebl, Chad Mitchell, Ryan Sandberg, Marco Garten, Ji Qiang, et al.'
 __license__: str = 'BSD-3-Clause-LBNL'
-__version__: str = '24.07'
+__version__: str = '24.08'
 s: impactx_pybind.CoordSystem  # value = <CoordSystem.s: 0>
 t: impactx_pybind.CoordSystem  # value = <CoordSystem.t: 1>
 cxx = impactx_pybind
