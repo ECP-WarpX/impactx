6132e65b5b81b8ede3721ab4c3978a2e75481b39
Chad Mitchell <46825199+cemitch99@users.noreply.github.com>
2024-08-28 20:43:30 -0500

    Update analysis_chicane_csr.py
    Relax tolerance 2.5 -> 3.5.

--- examples/chicane/analysis_chicane_csr.py
+++ examples/chicane/analysis_chicane_csr.py
@@ -61,7 +61,7 @@ print(
 )
 
 atol = 0.0  # ignored
-rtol = 2.5 * num_particles**-0.5  # from random sampling of a smooth distribution
+rtol = 3.5 * num_particles**-0.5  # from random sampling of a smooth distribution
 print(f"  rtol={rtol} (ignored: atol~={atol})")
 
 assert np.allclose(
@@ -88,7 +88,7 @@ print(
 )
 
 atol = 0.0  # ignored
-rtol = 2.5 * num_particles**-0.5  # from random sampling of a smooth distribution
+rtol = 3.5 * num_particles**-0.5  # from random sampling of a smooth distribution
 print(f"  rtol={rtol} (ignored: atol~={atol})")
 
 assert np.allclose(
