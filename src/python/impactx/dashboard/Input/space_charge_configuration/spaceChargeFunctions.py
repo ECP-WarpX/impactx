0b1f8ab38a4cb8b67fe7e3f9329d3add0bb2def0
Parthib Roy <proy@lbl.gov>
2024-08-31 21:41:12 -0700

    Add initial validation for Space Charge

---
+++ src/python/impactx/dashboard/Input/space_charge_configuration/spaceChargeFunctions.py
@@ -0,0 +1,36 @@
+from ...trame_setup import setup_server
+
+server, state, ctrl = setup_server()
+
+# -----------------------------------------------------------------------------
+# Functions
+# -----------------------------------------------------------------------------
+
+class SpaceChargeFunctions:
+
+    def validate_prob_relative_fields(index, prob_relative_value):
+        """
+        This function checks specific validation requirements
+        for prob_relative_fields.
+        :param index: The index of the prob_relative_field modified.
+        :param prob_relative_value: The numerical value entered by the user.
+        :return: An error message. An empty string if there is no error.
+        """
+        error_message = ""
+
+        try:
+            prob_relative_value = float(prob_relative_value)
+            poisson_solver = state.poisson_solver
+
+            if poisson_solver == "multigrid":
+                if index == 0 and prob_relative_value < 3:
+                    error_message = "Must be greater than 3."
+                elif index > 0 and prob_relative_value <= 1:
+                    error_message = "Must be greater than 1."
+            elif poisson_solver == "fft":
+                if prob_relative_value <= 1:
+                    error_message = "Must be greater than 1."
+        except ValueError:
+            error_message = "Must be a float."
+
+        return error_message
--- src/python/impactx/dashboard/Input/space_charge_configuration/spaceChargeMain.py
+++ src/python/impactx/dashboard/Input/space_charge_configuration/spaceChargeMain.py
@@ -1,6 +1,7 @@
 from trame.widgets import vuetify
 
 from ..generalFunctions import generalFunctions
+from .spaceChargeFunctions import SpaceChargeFunctions
 from ...trame_setup import setup_server
 
 server, state, ctrl = setup_server()
@@ -30,10 +31,8 @@ def populate_prob_relative_fields(max_level):
 
     state.prob_relative_fields = [
         {
-            "label": f"prob_relative{i+1}",
             "value": "",
-            "type": "number",
-            "error_message": "",
+            "error_message": SpaceChargeFunctions.validate_prob_relative_fields(i, ""),
         }
         for i in range(num_prob_relative_fields)
     ]
@@ -43,6 +42,23 @@ def populate_prob_relative_fields(max_level):
 # -----------------------------------------------------------------------------
 # Decorators
 # -----------------------------------------------------------------------------
+@state.change("poisson_solver")
+def on_poisson_solver_change(poisson_solver, **kwargs):
+    updated_prob_relative_fields = []
+
+    for i, field in enumerate(state.prob_relative_fields):
+        prob_relative_value = state.prob_relative[i]
+        error_message = SpaceChargeFunctions.validate_prob_relative_fields(i, prob_relative_value)
+
+        updated_field = {
+            "value": field["value"],
+            "error_message": error_message
+        }
+
+        updated_prob_relative_fields.append(updated_field)
+
+    state.prob_relative_fields = updated_prob_relative_fields
+    state.dirty("prob_relative_fields")
 
 @state.change("space_charge")
 def on_space_charge_change(space_charge, **kwargs):
@@ -54,26 +70,36 @@ def on_max_level_change(max_level, **kwargs):
 
 @state.change("n_cell_x", "n_cell_y", "n_cell_z")
 def on_nCell_value_change(n_cell_x, n_cell_y, n_cell_z, **kwargs):
-    # modify into an array of ints
+    state.error_message_x = generalFunctions.validate_against(n_cell_x, "int")
+    state.error_message_y = generalFunctions.validate_against(n_cell_y, "int")
+    state.error_message_z = generalFunctions.validate_against(n_cell_z, "int")
+
     state.n_cell = [
-        int(n_cell_x) if n_cell_x else 0.0,
-        int(n_cell_y) if n_cell_y else 0.0,
-        int(n_cell_z) if n_cell_z else 0.0,
+        int(n_cell_x) if not state.error_message_x else 0,
+        int(n_cell_y) if not state.error_message_y else 0,
+        int(n_cell_z) if not state.error_message_z else 0,
     ]
+
+    state.dirty("n_cell")
 
 @ctrl.add("update_prob_relative")
 def on_update_prob_relative_call(index, value):
-    index = int(index)
-    if index < len(state.prob_relative):
-        state.prob_relative[index] = float(value) if value else 0.0
-        state.prob_relative_fields[index]["value"] = str(state.prob_relative[index])
-        print(f"Updated prob_relative: {state.prob_relative}")
+    prob_relative_value, input_type = generalFunctions.determine_input_type(value)
 
+    index = int(index)
+
+    # Validate the updated value
+    error_message = SpaceChargeFunctions.validate_prob_relative_fields(index, prob_relative_value)
+
+    if index < len(state.prob_relative):
+        state.prob_relative[index] = prob_relative_value if value else 0.0
+        print(f"Updated prob_relative: {state.prob_relative}")
+        state.prob_relative_fields[index]["error_message"] = error_message
+        state.dirty("prob_relative_fields")
 
 # -----------------------------------------------------------------------------
 # UI
 # -----------------------------------------------------------------------------
-
 
 class SpaceChargeConfiguration:
     @staticmethod
@@ -126,6 +152,7 @@ class SpaceChargeConfiguration:
                         vuetify.VTextField(
                             placeholder="x",
                             v_model=("n_cell_x",),
+                            error_messages=("error_message_x",),
                             type="number",
                             dense=True,
                         )
@@ -133,6 +160,7 @@ class SpaceChargeConfiguration:
                         vuetify.VTextField(
                             placeholder="y",
                             v_model=("n_cell_y",),
+                            error_messages=("error_message_y",),
                             type="number",
                             dense=True,
                         )
@@ -140,6 +168,7 @@ class SpaceChargeConfiguration:
                         vuetify.VTextField(
                             placeholder="z",
                             v_model=("n_cell_z",),
+                            error_messages=("error_message_z",),
                             type="number",
                             dense=True,
                         )
@@ -156,6 +185,7 @@ class SpaceChargeConfiguration:
                             placeholder=("val."),
                             v_model=("field.value",),
                             input=(ctrl.update_prob_relative, "[index, $event]"),
+                            error_messages=("field.error_message",),
                             dense=True,
                             type="number",
                         )
