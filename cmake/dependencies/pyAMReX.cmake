36eb3b8615d16964b96d9f8849faf5fd21242a66
Axel Huebl <axel.huebl@plasma.ninja>
2024-10-10 09:15:10 -0700

    Release: 24.10 (#733)

--- cmake/dependencies/pyAMReX.cmake
+++ cmake/dependencies/pyAMReX.cmake
@@ -59,7 +59,7 @@ function(find_pyamrex)
         endif()
     elseif(NOT ImpactX_pyamrex_internal)
         # TODO: MPI control
-        find_package(pyAMReX 24.09 CONFIG REQUIRED)
+        find_package(pyAMReX 24.10 CONFIG REQUIRED)
         message(STATUS "pyAMReX: Found version '${pyAMReX_VERSION}'")
     endif()
 endfunction()
@@ -74,7 +74,7 @@ option(ImpactX_pyamrex_internal "Download & build pyAMReX" ON)
 set(ImpactX_pyamrex_repo "https://github.com/AMReX-Codes/pyamrex.git"
     CACHE STRING
     "Repository URI to pull and build pyamrex from if(ImpactX_pyamrex_internal)")
-set(ImpactX_pyamrex_branch "1c66690f83244196c5655293f1381303a7d1589d"
+set(ImpactX_pyamrex_branch "24.10"
     CACHE STRING
     "Repository branch for ImpactX_pyamrex_repo if(ImpactX_pyamrex_internal)")
 
